fb7734c4bf0f191a521b5d3ee4e1771d4306df5b
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-26 00:39:09 +0530

    Added : AddressBook JAVA

---
+++ README.md
@@ -0,0 +1,1 @@
+# Adv-Address-Book-System
---
+++ src/main/java/advaddressbook/AddressBookException.java
@@ -0,0 +1,14 @@
+package advaddressbook;
+
+public class AddressBookException extends Exception {
+    enum ExceptionType {
+        DatabaseException, NoSuchClass, ResourcesNotClosedException, ConnectionFailed, CommitFailed
+    }
+
+    public ExceptionType type;
+
+    public AddressBookException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+}
